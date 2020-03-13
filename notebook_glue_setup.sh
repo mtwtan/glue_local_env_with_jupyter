ce6d529e36fed4334c1a16be9b1434534012ebb4
mtwtan <mtwtan@gmail.com>
2020-03-13 15:59:14 -0400

    Update notebook_glue_setup.sh

--- notebook_glue_setup.sh
+++ notebook_glue_setup.sh
@@ -1,8 +1,8 @@
-# Install packages
-sudo yum -y install java-1.8.0-openjdk.x86_64 git gcc python3 python3-devel
-sudo amazon-linux-extras install nginx1.12 -y
 
-cd ~
+# Install packages
+sudo yum -y install java-1.8.0-openjdk.x86_64 git gcc python3 python3-devel nginx
+
+cd $HOME
 
 # Download glue library and set to version glue-1.0
 git clone https://github.com/awslabs/aws-glue-libs.git
@@ -10,7 +10,7 @@ cd aws-glue-libs/
 git checkout glue-1.0
 
 # Download Maven
-cd ~
+cd $HOME
 curl -o apache-maven-3.6.0-bin.tar.gz https://aws-glue-etl-artifacts.s3.amazonaws.com/glue-common/apache-maven-3.6.0-bin.tar.gz
 tar -xvf apache-maven-3.6.0-bin.tar.gz
 
@@ -18,45 +18,45 @@ tar -xvf apache-maven-3.6.0-bin.tar.gz
 curl -o spark-2.4.3-bin-hadoop2.8.tgz https://aws-glue-etl-artifacts.s3.amazonaws.com/glue-1.0/spark-2.4.3-bin-hadoop2.8.tgz
 tar -xvf spark-2.4.3-bin-hadoop2.8.tgz
 
-# Backup original .bash_profile
-cp .bash_profile .bash_profile_bak
+# Backup original $HOME/.bash_profile
+cp $HOME/.bash_profile $HOME/.bash_profile_bak
 
-#### Configure .bash_profile to set the appropriate environmental variables
+#### Configure $HOME/.bash_profile to set the appropriate environmental variables
 
 # Add maven to PATH
-echo "PATH=\$PATH:/home/ec2-user/apache-maven-3.6.0/bin" >> .bash_profile
+echo "PATH=\$PATH:/home/ec2-user/apache-maven-3.6.0/bin" >> $HOME/.bash_profile
 
 # Set SPARK_HOME
-echo "export SPARK_HOME=/home/$USER/spark-2.4.3-bin-spark-2.4.3-bin-hadoop2.8" >> .bash_profile
+echo "export SPARK_HOME=/home/$USER/spark-2.4.3-bin-spark-2.4.3-bin-hadoop2.8" >> $HOME/.bash_profile
 
 # Set JAVA_HOME
 ####java=$(alternatives --display java | grep "link currently points to")
 java=$(alternatives --display java | grep link | sed 's/^ link.*to //')
 java2=$(echo $java | sed 's/link currently points to //')
 jvmhome=$(echo $java2 | sed 's/\/bin\/java//')
-echo "export JAVA_HOME=$jvmhome" >> .bash_profile
+echo "export JAVA_HOME=$jvmhome" >> $HOME/.bash_profile
 
 # Set python3 as default python language
-echo "alias python=python3" >> .bash_profile
+echo "alias python=python3" >> $HOME/.bash_profile
 
 # Configuration setting for PYSPARK
-echo "export SPARK_CONF_DIR=$HOME/aws-glue-libs/conf" >> .bash_profile
-echo "export PYTHONPATH=\"${SPARK_HOME}python/:${SPARK_HOME}python/lib/py4j-0.10.7-src.zip:$HOME/bin/aws-glue-libs/PyGlue.zip:${PYTHONPATH}\"" >> .bash_profile
+echo "export SPARK_CONF_DIR=$HOME/aws-glue-libs/conf" >> $HOME/.bash_profile
+echo "export PYTHONPATH=\"${SPARK_HOME}python/:${SPARK_HOME}python/lib/py4j-0.10.7-src.zip:$HOME/bin/aws-glue-libs/PyGlue.zip:${PYTHONPATH}\"" >> $HOME/.bash_profile
 
-echo "export PYSPARK_DRIVER_PYTHON=\"jupyter\"" >> .bash_profile
-echo "export PYSPARK_DRIVER_PYTHON_OPTS=\"notebook\"" >> .bash_profile
-echo "export PYSPARK_PYTHON=python3" >> .bash_profile
-echo "export PATH=${SPARK_HOME}bin:$PATH:~/.local/bin:$JAVA_HOME/bin:$JAVA_HOME/jre/bin" >> .bash_profile
+echo "export PYSPARK_DRIVER_PYTHON=\"jupyter\"" >> $HOME/.bash_profile
+echo "export PYSPARK_DRIVER_PYTHON_OPTS=\"notebook\"" >> $HOME/.bash_profile
+echo "export PYSPARK_PYTHON=python3" >> $HOME/.bash_profile
+echo "export PATH=${SPARK_HOME}bin:$PATH:~/.local/bin:$JAVA_HOME/bin:$JAVA_HOME/jre/bin" >> $HOME/.bash_profile
 
 # Pull in the new environment settings
-source .bash_profile
+source $HOME/.bash_profile
 
 
 # Upgrade pip
 sudo pip3 install pip --upgrade
 
 # Install jupyter
-cd ~
+cd $HOME
 pip install jupyter
 pip install git+https://github.com/sat28/githubcommit.git
 pip install requests
