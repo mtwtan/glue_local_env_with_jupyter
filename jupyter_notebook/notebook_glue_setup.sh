ad28df6b72d3410a068c5f3d9cb19ce220a578c1
mtwtan <mtwtan@gmail.com>
2020-03-25 20:33:30 -0400

    Update notebook_glue_setup.sh

--- jupyter_notebook/notebook_glue_setup.sh
+++ jupyter_notebook/notebook_glue_setup.sh
@@ -89,8 +89,8 @@ echo "c.NotebookApp.port = 8000" >> $HOME/.jupyter/jupyter_notebook_config.py
 
 # Modify NGINX configuration
 sudo mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.orig
-sudo cp ~/glue_local_env_with_jupyter/nginx.conf /etc/nginx/nginx.conf
-sudo cp ~/glue_local_env_with_jupyter/notebook.conf /etc/nginx/conf.d/notebook.conf
+sudo cp ~/glue_local_env_with_jupyter/common/nginx.conf /etc/nginx/nginx.conf
+sudo cp ~/glue_local_env_with_jupyter/common/notebook.conf /etc/nginx/conf.d/notebook.conf
 sudo systemctl enable nginx.service
 sudo systemctl start nginx.service
 
